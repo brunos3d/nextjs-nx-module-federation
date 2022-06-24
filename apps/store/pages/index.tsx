3bec3c083ab8a3fc9c45e20fe447be57f62b9389
Bruno Silva <bruno3dcontato@gmail.com>
2022-06-24 13:26:18 -0300

    feat: add hook example

--- apps/store/pages/index.tsx
+++ apps/store/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import dynamic from 'next/dynamic';
 import styles from './index.module.css';
 
@@ -7,12 +8,37 @@ const BuyButton = dynamic(async () => import('checkout/buy-button'), {
   ssr: false,
 });
 
+type UseAddToCartHookType = () => UseAddToCartHookResultType;
+
+type UseAddToCartHookResultType = {
+  itemsCount: number;
+  addToCart: () => void;
+  clearCart: () => void;
+};
+
+let useAddToCartHook: UseAddToCartHookType;
+
+if (process.browser) {
+  useAddToCartHook = require('checkout/useAddToCartHook').default;
+}
+
 export function Home() {
+  let cartHook = {} as UseAddToCartHookResultType;
+
+  if (process.browser) {
+    cartHook = useAddToCartHook();
+  }
+  const { itemsCount, addToCart, clearCart } = cartHook;
+
   return (
     <div className={styles['container']}>
       <h1>Welcome to Store!</h1>
 
-      <BuyButton>Add to Cart</BuyButton>
+      <p>
+        Item Count: <strong>{itemsCount}</strong>
+      </p>
+      <BuyButton onClick={addToCart}>Add to Cart</BuyButton>
+      <BuyButton onClick={clearCart}>Clear Cart</BuyButton>
     </div>
   );
 }
