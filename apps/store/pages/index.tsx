18240c247f2b197d56c81d264b957ddbbec74417
Bruno Silva <bruno3dcontato@gmail.com>
2022-06-27 08:51:27 -0300

    feat: add module-fedaration link

--- apps/store/pages/index.tsx
+++ apps/store/pages/index.tsx
@@ -4,9 +4,10 @@ import styles from './index.module.css';
 export function Page() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Module Federation!</h1>
+      <h1>Welcome to Next.js, Nx, Module Federation!</h1>
 
       <Link href="/design-system">design-system</Link>
+      <Link href="/module-federation">module-federation</Link>
     </div>
   );
 }
