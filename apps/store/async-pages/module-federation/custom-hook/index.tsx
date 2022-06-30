b5bc75c1f1ec1a3d8bd4124963102c5a9d5e9815
Bruno Silva <bruno3dcontato@gmail.com>
2022-06-30 15:05:17 -0300

    feat: improve custom-hook example layout

--- apps/store/async-pages/module-federation/custom-hook/index.tsx
+++ apps/store/async-pages/module-federation/custom-hook/index.tsx
@@ -23,11 +23,22 @@ export function Page() {
     <div className={styles['container']}>
       <h1>Welcome to Custom Hook!</h1>
 
-      <p>
+      <p className="mt-5">
         Item Count: <strong>{itemsCount}</strong>
       </p>
-      <button onClick={addToCart}>Add to Cart</button>
-      <button onClick={clearCart}>Clear Cart</button>
+
+      <div className="flex mt-3">
+        <button type="button" className="btn btn-primary" onClick={addToCart}>
+          Add to Cart
+        </button>
+        <button
+          type="button"
+          className="btn btn-danger ms-2"
+          onClick={clearCart}
+        >
+          Clear Cart
+        </button>
+      </div>
     </div>
   );
 }
